41322f14de9071d5fb3c45622f59e2e9192f3977
zombonline <megazombie130@gmail.com>
2022-11-27 15:59:38 +0000

    stationary enemy cont.

--- Assets/Scripts/StationaryEnemy.cs
+++ Assets/Scripts/StationaryEnemy.cs
@@ -5,17 +5,50 @@ using UnityEngine;
 public class StationaryEnemy : MonoBehaviour
 {
     [SerializeField] float sightLength;
+    [SerializeField] LayerMask layersToCheck;
+
+    [SerializeField] GameObject projectilePrefab;
+    Transform target;
+    [SerializeField] float projectileForce = 50f;
+    [SerializeField] float projectileCooldown;
+    float coolDownTimer;
+
+    private void Update()
+    {
+        coolDownTimer -= Time.deltaTime;
+    }
+
     private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            Vector2 dir = (this.transform.position - collision.transform.position);
-            Debug.Log(dir);
-            RaycastHit2D ray = Physics2D.Raycast(transform.position, dir, sightLength);
-            if(ray.collider != null)
+            RaycastHit2D hit = Physics2D.Linecast
+                    (transform.position, collision.transform.position, layersToCheck);
+            Debug.DrawLine(transform.position, collision.transform.position);
+            if (hit.collider)
             {
-                Debug.Log(ray.collider.name);
+                if (hit.collider.gameObject.CompareTag("Player"))
+                {
+                    target = hit.collider.transform;
+                    if(coolDownTimer < 0)
+                    {
+                        Shoot();
+                        coolDownTimer = projectileCooldown;
+                    }
+                }
+                else
+                {
+                    target = null;
+                }
             }
         }
     }
+
+    void Shoot()
+    {
+        var newProjectile = Instantiate(projectilePrefab, transform.position, Quaternion.identity);
+        var dir = target.position - transform.position;
+        newProjectile.GetComponent<Rigidbody2D>().AddForce(dir * projectileForce);
+    }
+
 }
