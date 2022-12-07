d8507809584a287cd6c6ffc827e4f7fca2b56a6c
DESKTOP-G9P28AG\megaz <megazombie130@gmail.com>
2022-12-07 19:29:27 +0000

    post testing tweaks began work on checkpoints new art assets

---
+++ Assets/Scripts/CheckpointManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class CheckpointManager : MonoBehaviour
+{
+    public Transform currentCheckpoint;
+    [SerializeField] GameObject player;
+    private void Awake()
+    {
+        //Do not destroy on load
+        int gameSessionCount = FindObjectsOfType<LevelWinCanvas>().Length;
+        if (gameSessionCount > 1)
+        {
+
+            gameObject.SetActive(false);
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+
+        player = GameObject.FindGameObjectWithTag("Player");
+        if(currentCheckpoint !=null)
+        {
+            player
+        }
+    }
+}
--- Assets/Scripts/Dirt.cs
+++ Assets/Scripts/Dirt.cs
@@ -11,19 +11,19 @@ public class Dirt : MonoBehaviour
     {
         timer = timeTilDamage;
     }
-
-    private void OnCollisionStay2D(Collision2D collision)
+    private void OnTriggerStay2D(Collider2D collision)
     {
-        if(collision.collider.CompareTag("Player"))
+        if (collision.CompareTag("Player"))
         {
             timer -= Time.deltaTime;
             if (timer < 0)
             {
-                collision.collider.GetComponent<Health>().UpdateHealth(-1);
+                collision.GetComponent<Health>().UpdateHealth(-1);
                 timer = timeTilDamage;
             }
         }
     }
+   
 
     private void OnCollisionExit2D(Collision2D collision)
     {
